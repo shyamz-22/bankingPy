0098cbd6e288a31cfcd8dc7bbd808fdfc6eba08e
Syamala Umamaheswaran <shykart2203@gmail.com>
2018-11-08 12:15:24 +0100

    remove trouble shooting print statements

--- home/views.py
+++ home/views.py
@@ -8,10 +8,6 @@ home_view = Blueprint('home', __name__, url_prefix='/home')
 @home_view.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    print('before_validate')
-    print(form.validate_on_submit())
     if form.validate_on_submit():
-        print('after_validate')
-        print(url_for('account.debit'))
         return redirect(url_for('account.debit'))
     return render_template('login.html', title='Login', form=form)
